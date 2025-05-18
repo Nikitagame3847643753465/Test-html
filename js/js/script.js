910f2f23b640f2240c9b6a4ea6ca970221b7482c
Nikitagame3847643753465 <starunov-nik@mail.ru>
2025-05-18 16:20:08 +0300

    новый проект

---
+++ js/js/script.js
@@ -0,0 +1,62 @@
+// number - число
+// BigInt - числа высокого разряда
+// string - строка/текст
+// boolean - true и false
+// null и undefined - неизвестное значение (ничего)
+
+//console.error(11232);
+//console.log("ewkfsekoc")
+
+//let a = 5; // объявление переменной
+//let b = 22; 
+
+//if (a > b){
+    //console.log("a больше b")
+//}/else{
+    //console.log("a меньше b")
+//}
+
+//let password = 1231231232;
+//let usePassword = 123123123;
+
+//if(password == usePassword){
+    //console.log("Доступ разрешён")
+//}else{
+   // console.error("доступ запрещён")
+///}
+
+// тернарный оператор - сокращенная версия if else
+
+
+//str = password == usePassword ? "доступ разрешён" : "доступ запрещён"
+//console.log(str);
+
+
+// let a = 1;
+// let b = 2; 
+// let c = 3;
+
+// или ||
+// и &&
+// нет !
+
+
+// if( a == c && b == c){
+//     console.log("правильно")
+// }else{
+//     console.error("неправильно")
+// }
+
+ // let a - объявление переменной
+// const a - не изменяемые переменные
+
+let a = 1
+a = 3
+console.log(a)  // выводет 3
+
+const b = 1 // будет ошибка
+b = 3
+console.log(a)
+
+// let - изменеяется
+// const - не изменяется
